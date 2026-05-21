88f487b52f5cfb80f5199e0c57dca0ccdbda3b41
Diana Darfl <diana.darfl@gmail.com>
2026-05-21 23:00:20 +0500

    added models(SuccessfulLoginResponse), full LoginTests

---
+++ src/test/java/models/SuccessfulLoginResponse.java
@@ -0,0 +1,9 @@
+package models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+public class SuccessfulLoginResponse {
+    String token;
+}
